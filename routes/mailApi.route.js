1c1c21b304367292b2e9b4fae9c3542bd440d1ba
alextrazah <mohamedaziz.sahnoun@esprit.tn>
2021-10-06 15:40:11 +0100

    challenge 4.3

--- routes/mailApi.route.js
+++ routes/mailApi.route.js
@@ -1,71 +1,99 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 var router = express.Router();
-const fs = require('fs');
-const path = require('path');
-const ejs = require('ejs');
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
 
-router.get('/mail-text', async(req, res)=>{
+router.post("/mail-text", async (req, res) => {
+  try {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "mohamedaziz.sahnoun@esprit.tn",
-            pass: "20791756"
-    }});
-    try{
-   await transporter.sendMail({
-        from: 'mohamedaziz.sahnoun@esprit.tn',
-        to: "mohamedaziz.sahnoun@esprit.tn",
-        subject: "Salut", 
-        text: req.body.text
+      service: "gmail",
+      auth: {
+        user: "mohamedaziz.sahnoun@esprit.tn",
+        pass: "20791756",
+      },
     });
-    res.json({message : "done"})
-}
-catch(err){ console.log(err);
-        res.status(500).json({message:"internal server err"});}
+    await transporter.sendMail({
+      from: "mohamedaziz.sahnoun@esprit.tn",
+      to: "mohamedaziz.sahnoun@esprit.tn",
+      subject: "Salut",
+      text: req.body.text,
+    });
+    res.json({ message: "done" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "internal server err" });
+  }
 });
 
-router.post('/mail-ejs', async(req, res)=>{
+router.post("/mail-ejs", async (req, res) => {
+  try {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "mohamedaziz.sahnoun@esprit.tn",
-            pass: "20791756"
-        }});
-    const template = fs.readFileSync(path.resolve('./views', 'sendmail.ejs'), {encoding: 'utf-8'});
+      service: "gmail",
+      auth: {
+        user: "mohamedaziz.sahnoun@esprit.tn",
+        pass: "20791756",
+      },
+    });
+    const template = fs.readFileSync(path.resolve("./views", "sendmail.html"), {
+      encoding: "utf-8",
+    });
     const html = ejs.render(template, {
-        text: req.body.text
-    })
+      name: req.body.name,
+    });
 
     let info = await transporter.sendMail({
-        from: 'mohamedaziz.sahnoun@esprit.tn', 
-        to: "mohamedaziz.sahnoun@esprit.tn",
-        subject: "test mailing via ejs",
-        html: html,
+      from: "mohamedaziz.sahnoun@esprit.tn",
+      to: "mohamedaziz.sahnoun@esprit.tn",
+      subject: "test mailing via ejs",
+      html: html,
     });
     // 4. send respone
-    res.json({message : "done"})
+    res.json({ message: "done" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "internal server err" });
+  }
 });
-router.post('/attachement-ejs', async(req, res)=>{
+router.post("/attachement-ejs", async (req, res) => {
+  try {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "mohamedaziz.sahnoun@esprit.tn",
-            pass: "20791756"
-        }});
+      service: "gmail",
+      auth: {
+        user: "mohamedaziz.sahnoun@esprit.tn",
+        pass: "20791756",
+      },
       
+    });
+
+
+    const template = fs.readFileSync(path.resolve("./views", "sendmail.html"), {
+      encoding: "utf-8",
+    });
+    const html = ejs.render(template, {
+      name: req.body.name,
+    });
 
     let info = await transporter.sendMail({
-        from: 'mohamedaziz.sahnoun@esprit.tn', 
-        to: "mohamedaziz.sahnoun@esprit.tn",
-        subject: "test mailing via ejs",
-        attachments: [{
-            filename: "filename",
-            path: 'C:/Users/aziz/Desktop/fivenode/fivenode/assets/edit.txt' 
-        }]
+      from: "mohamedaziz.sahnoun@esprit.tn",
+      to: "mohamedaziz.sahnoun@esprit.tn",
+      subject: "test mailing via ejs",
+      html: html,
+      attachments: [
+        {
+          filename: "filename.png",
+          path: path.resolve("./public/assets/fivepoints.png"),
+        },
+      ],
+    
     });
     // 4. send respone
-    res.json({message : "done"})
+    res.json({ message: "done" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "internal server err" });
+  }
 });
 
 module.exports = router;
--- routes/uploadFileApi.route.js
+++ routes/uploadFileApi.route.js
@@ -1,39 +1,29 @@
 const express = require('express');
 
 const multer = require('multer');
-const util = require("util");
 const path = require("path");
-const fs = require('fs');
-const { execSync } = require("child_process");
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        if (
-            !fs.existsSync(
-                path.join(__dirname, '../public/uploads')
-            )
-        ) {
-            execSync(
-                `mkdir "${path.join(
-                    __dirname, '../public/uploads'
-                )}"`
-            );
-        }
+    destination:  (req, file, cb) =>{
         cb(null, './public/uploads');
     },
-    filename: function (req, file, cb) {
+    filename:  (req, file, cb)=> {
        // console.log(file);
-        cb(null, Date.now() + file.originalname);
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'|| file.mimetype === 'image/jpg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
+    const alowedextentions = ['.png','.jpg','.gif','.jpeg' ]
+    const fileExtention=  path.extname(file.originalname);
+    cb(null, alowedextentions.includes(fileExtention));
+
+    // if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'|| file.mimetype === 'image/jpg') {
+    //     cb(null, true);
+    // } else {
+    //     cb(null, false);
+    // }
 }
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
@@ -41,7 +31,10 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 
 
-router.post('/uploadfile', upload.single('image'), async(req, res)=>{
+router.post('/upload-file', upload.single('image'), async(req, res)=>{
+    res.json({message: 'done'});
+});
+router.post('/upload-file-Multi', upload.array('image', 5), async(req, res)=>{
     res.json({message: 'done'});
 });
 
